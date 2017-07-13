2dee6300b52a27efda4bb65b684e2faa8e1b0138
Dylan Cunningham <gdylanc@gmail.com>
2017-07-13 14:57:50 -0700

    module one complete

---
+++ splash.js
@@ -0,0 +1,34 @@
+function readyFn( jQuery ) {
+    console.log("here we go")
+
+
+
+
+
+
+console.log($(".test"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+$( window ).on( "load", readyFn );
