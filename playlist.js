8371edf66c6d7ee454a7a91c3b91718d24c005bb
Dylan Cunningham <gdylanc@gmail.com>
2017-07-14 11:00:55 -0700

    module 4 completed

--- playlist.js
+++ playlist.js
@@ -1,74 +1,61 @@
 function readyFn( jQuery ) {
     console.log("here we go")
 
-  // function to generate 3 nonequal random nums. n=how many elements you want the array to generate //
-  function getXRandomInts(min, max, n) {
-    array = [];
-    while (array.length < n){
-      var num = Math.floor(Math.random() * (max - min + 1)) + min;
-      if (num !== array[0] && num !== array[1]){
-        array.push(num);
+
+  function getRandomLookingArray(min, max, n) {
+   // n is the number of elements in array
+   // min is the minimum possible value
+   // max is the max posible value
+  array = [];
+  while (array.length < n){
+    var num = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (num !== array[array.length-1] && num !== array[array.length-2] ){
+      array.push(num);
       }
     }
     return array;
   }
 
 
-
-
-
-  // loads pics on splash page with page loading as the event listener
+  // populates the album divs on page load
   $( document ).ready( function() {
     // lets get the info from the api
       $.get('https://lit-fortress-6467.herokuapp.com/object', function(data){
-        console.log(data);
-      var counter = 0;
-      while (counter < 5) {
-        for (var i=0; i<(data['results'].length); i++) {
-          var albumName = data['results'][i]['cover_art'];
-          var newImg = new Image(130,130);
-          // var newImg = document.createElement('img');
-          newImg.src = albumName;
-          $(".albumholder").append(newImg);
-        }
-        counter ++;
+
+      // let's make an array of 15 numbers that doesn't look repetitive at a glance
+      var randomLookingArray = getRandomLookingArray(0,4,15);
+      // this loop is appending new images to the .albumholder div in a way that doesn't look repetitive at a glance
+      for (var i=0; i<randomLookingArray.length; i++) {
+        var albumName = data['results'][randomLookingArray[i]]['cover_art'];
+        var newImg = new Image(130,130);
+        // var newImg = document.createElement('img');
+        newImg.src = albumName;
+        $(".albumholder").append(newImg);
       }
-      var selectedArtTit;
-      var allImg = $('img')
-      $(allImg).click(function(event){
-        var albumMatch = ($(this).attr('src'));
-        console.log(albumMatch)
-        var drillAlbumName = data['results'][j];
-        for (var j=0; j<data['results'].length; j++){
 
-          // console.log(data['results'][j]['cover_art'])
-          if (albumMatch == data['results'][j]['cover_art']){
-            selectedArtTit = (data['results'][j]['artist'] +': '+ data['results'][j]['title'] )
+
+        var selectedArtTit; // selected artist title info
+        var allImg = $('img')  // a selector for all of the <img>s
+        // click event for when any img gets clicked
+        $(allImg).click(function(event){
+          // this variable stores the data that allows our loop to identify which object is clicked upon.
+          var albumMatch = ($(this).attr('src'));
+          // now that we have the data to compare against, let's find the matching src info from an object in our loop
+          for (var j=0; j<data['results'].length; j++){
+            // we'll look thru all of objects cover art value and if it is equal to our albumMatch variable, we'll assign the selectedArtTit variable with that objects artist: title info.
+            if (albumMatch == data['results'][j]['cover_art']){
+              selectedArtTit = (data['results'][j]['artist'] +': '+ data['results'][j]['title'] )
+            }
           }
-
-
-        }
-        var infoToAppend = document.createElement('h4');
-        infoToAppend.innerText = selectedArtTit;
-        $('.texthere').append(infoToAppend);
-
-
+          // create new <h4> to append in our grey box
+          var infoToAppend = document.createElement('h4');
+          // set that <h4>'s inner text to selectedArtTit'
+          infoToAppend.innerText = selectedArtTit;
+          // now append it to the flex-container that's classed as .texthere
+          $('.texthere').append(infoToAppend);
+        })
       })
-
-
-      })
-    })
+  })
 }
-
-
-
-
-
-
-
-
-
-
-
 // this is the end of the ready function we always run with jQuery.
 $( window ).on( "load", readyFn );
